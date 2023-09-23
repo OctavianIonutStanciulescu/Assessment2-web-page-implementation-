22c9041722f6dc0172146914e569763d0c2834a5
OctavianIonutStanciulescu <86408475+OctavianIonutStanciulescu@users.noreply.github.com>
2023-09-23 22:51:36 +0530

    Update app.py

--- web-prototype/app.py
+++ web-prototype/app.py
@@ -2,13 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Sample student data (you can replace this with your own data)
-students = [
-    {"name": "John Doe", "major": "Computer Science"},
-    {"name": "Jane Smith", "major": "Engineering"},
-    {"name": "Alice Johnson", "major": "Biology"},
-]
-
 @app.route("/")
 def home():
     return render_template("index.html")
